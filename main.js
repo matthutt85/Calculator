bb1120e33418e26ec64206d2ef7d22533fa5b85c
matthutt85 <matt.hutt85@gmail.com>
2021-09-14 14:39:25 +0100

    variables added to main js file

--- main.js
+++ main.js
@@ -1,1 +1,6 @@
-const numberButtons = document.querySelector("")
+const numberButtons = document.querySelectorAll("[data-number]");
+const display = document.querySelector("[data-display");
+const operattionButton = document.querySelectorAll("[operation-button]");
+const equalsButton = document.querySelector("[data-equals]");
+const delButton = document.querySelector("[data-del]");
+const clearAllButton = document.querySelector("[data-clear-all]");
