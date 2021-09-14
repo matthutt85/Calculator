2d0a7b13f3be53051b2a8e6c10de0fc9cf7703bb
matthutt85 <matt.hutt85@gmail.com>
2021-09-14 17:16:37 +0100

    change to the js code, still stuck!

--- main.js
+++ main.js
@@ -1,6 +1,12 @@
-const numberButtons = document.querySelectorAll("[data-number]");
-const display = document.querySelector("[data-display");
-const operattionButton = document.querySelectorAll("[operation-button]");
-const equalsButton = document.querySelector("[data-equals]");
-const delButton = document.querySelector("[data-del]");
-const clearAllButton = document.querySelector("[data-clear-all]");
+const numbersButtons = document.querySelectorAll(".numbers");
+const display = document.querySelector(".screen__bottom")
+
+
+numbersButtons.forEach(number => {
+    number.addEventListener('click', (event) => {
+      console.log(event.target.innerHTML)
+    }); 
+})
+
+
+
