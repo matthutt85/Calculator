cee83ffb1fd3e5a8314dc90660efa60e075ba9d9
matthutt85 <matt.hutt85@gmail.com>
2021-09-15 15:14:28 +0100

    numbers coming up on screen

--- main.js
+++ main.js
@@ -1,12 +1,26 @@
 const numbersButtons = document.querySelectorAll(".numbers");
-const display = document.querySelector(".screen__bottom")
+const display = document.querySelector(".screen__bottom");
+const operationButtons = document.querySelectorAll(".operation-button");
+const del = document.querySelector(".del");
+const allClear = document.querySelector(".all-clear");
+const equals = document.querySelector(".equals");
 
+let numberInput;
+let operationInput;
 
 numbersButtons.forEach(number => {
     number.addEventListener('click', (event) => {
-      console.log(event.target.innerHTML)
+      numberInput = event.target.innerText;
+      display.textContent = numberInput
+      console.log(numberInput)
     }); 
 })
+      
 
-
+operationButtons.forEach(operation => {
+  operation.addEventListener('click', (event) => {
+    operationInput = event.target.innerText;
+    display.textContent = operationInput;
+  });
+})
 
