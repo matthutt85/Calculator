1c26a94ba02becb3348bb69dbcac4059fbf82e65
matthutt85 <matt.hutt85@gmail.com>
2021-09-16 13:44:35 +0100

    can't use eval, need new way

--- main.js
+++ main.js
@@ -1,30 +1,54 @@
 const numbersButtons = document.querySelectorAll(".numbers");
-const display = document.querySelector(".screen__bottom");
+const display = document.querySelector(".display__bottom");
+const display2 = document.querySelector(".display__top")
+const tempDisplay = document.querySelector(".display__temp-result")
 const operationButtons = document.querySelectorAll(".operation-button");
 const del = document.querySelector(".del");
 const allClear = document.querySelector(".all-clear");
 const equals = document.querySelector(".equals");
 
-let displayInput = '';
+let displayInput1 = '';
+let displayInput2 = '';
+let tempDisplay1 = '';
+let result = null;
+let lastOperation = '';
+
 
 numbersButtons.forEach(number => {
   number.addEventListener('click', (event) => {
-    displayInput += event.target.innerText;
-    display.textContent = displayInput;
+    displayInput1 += event.target.innerText;
+    display.textContent = displayInput1;
   }); 
 })
 
 
 operationButtons.forEach(operation => {
   operation.addEventListener('click', (e) => {
-    displayInput += e.target.innerText;
-    display.textContent = displayInput;
-  })
-})
+    if (!displayInput1) return;
+    const operationName = e.target.innerText;
+    if (displayInput1 && displayInput2 && lastOperation) {
+      sum();
+    } else {
+      result = parseFloat(displayInput1);
+    }
+    clearOp(operationName);
+    console.log(result);
+    // displayInput += e.target.innerText;
+    // display.textContent = displayInput;
+  });
+});
+
+function clearOp (name = '') {
+  displayInput2 += displayInput1 + ' ' + name + ' ';
+  display2.innerText = displayInput2;
+  display.innerText = '';
+  displayInput1 = '';
+}
+
 
 equals.addEventListener('click', (e) => {
-  let sum = display.textContent;
-  display.textContent = eval(sum);
+  // let sum = display.textContent;
+  // display.textContent = eval(sum);
 });
 
 del.addEventListener('click', (e) => {
@@ -33,5 +57,7 @@ del.addEventListener('click', (e) => {
 
 allClear.addEventListener('click', (e) => {
   display.textContent = '';
-  displayInput = '';
+  display2.textContent = '';
+  displayInput1 = '';
+  displayInput2 = '';
 });
